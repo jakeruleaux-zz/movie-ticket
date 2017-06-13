5edd0476691d4962873fd545ac480f762a3ee416
jacob ruleaux and Nathan Stewart <jacob@>
2017-06-13 15:28:08 -0700

    make it do stuff

--- js/scripts.js
+++ js/scripts.js
@@ -4,35 +4,35 @@ function Ticket(movie, time, age) {
   this.age = age;
 }
 
+var ticketPrice = [];
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+var resultCalc = function(inputTime, inputAge) {
+  console.log(inputAge);
+  if (inputTime >= 1 && inputTime <= 5) {
+    ticketPrice.push("3.00");
+  } else if (inputTime >= 6 && inputTime <= 11 && inputAge >= 1 && inputAge <= 16) {
+    ticketPrice.push("7.00");
+  } else if (inputTime >= 6 && inputTime <= 11 && inputAge >= 55 && inputAge <= 100) {
+    ticketPrice.push("8.00");
+  } else {
+    ticketPrice.push("10.00");
+  }
+};
 
 $(document).ready(function() {
   $("form#goer-input").submit(function(event) {
     event.preventDefault();
 
+    //$("div#price").empty();
+
     var inputtedMovie = $("input#movie-name").val();
     var inputTime = $("input#time").val();
     var inputAge = $("input#age").val();
+    var result = resultCalc(inputTime, inputAge);
 
-    var newTicket =  new Ticket(inputtedMovie, inputTime, inputAge);
+    var newTicket = new Ticket(inputtedMovie, inputTime, inputAge);
 
-    $("div#price").append("<p><span class='ticket'>" + newTicket.movieName + "</span></p>");
-
+    $("#result").show("div#price").append("<p><span class='ticket'>" + ticketPrice + "</span></p>");
 
   });
 });
